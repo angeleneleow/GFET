161bfcb51140aae3047e58b7d16f149156e6918e
Angeline Leow <aleow@minerva.local>
2019-07-12 14:48:14 -0700

    enable - sign for TDX

--- GeoToolkit/Mag/DataIO.py
+++ GeoToolkit/Mag/DataIO.py
@@ -344,7 +344,7 @@ class dataGrid(object):
           if getattr(self, '_TDXderivative', None) is None:
 
               self._TDXderivative = np.arctan2(
-                self.totalHorizontal, self.firstVertical
+                np.sign(self.firstVertical)*self.totalHorizontal, -self.firstVertical
             )*180/np.pi
 
           return self._TDXderivative
